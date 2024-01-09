b07ffd3f098634784f4d338f18d12a08fea2a879
hitinsharma <hitinsharma@yahoo.com>
2024-01-09 20:52:23 +0530

    second commit

--- project2/connection.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php
-//change mysqli_connect(host_name,username, password); 
-$connection = mysqli_connect("localhost", "hitintec_sharmaji", "Hcvbotp123@#", "hitintec_mydb");
-//$db = mysqli_select_db($connection, 'hitintec_mydb');
-?>
